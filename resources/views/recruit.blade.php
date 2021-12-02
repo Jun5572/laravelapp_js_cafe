5eba790e35ae1faaa6ca0568ff97d5173e2b7099
= <takizawa.tab@gmail.com>
2021-12-02 23:53:40 +0900

    スタイル微調整　リクルートページレイアウト化

--- resources/views/recruit.blade.php
+++ resources/views/recruit.blade.php
@@ -1,38 +1,14 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('css/reset.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/common.css') }}">
+@extends('layouts.base')
+
+@section('pageCss')
     <link rel="stylesheet" href="{{ asset('css/recruit.css') }}">
-    <script src="https://kit.fontawesome.com/d9203d9956.js" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
-    <title>J's Cafe</title>
-</head>
-<body>
-    <section class="hero w-container">
-        <div class="hero-img"></div>
-        <div class="hero-container">
-            <header class="header">
-                <a href="../top/top.html" class="logo">logo</a>
-                <nav>
-                    <ul>
-                        <li><a href="../news/news.html">NEWS</a></li>
-                        <li><a href="../menu/menu.html">MENU</a></li>
-                        <li><a href="../concept/concept.html">CONCEPT</a></li>
-                    </ul>
-                </nav>
-            </header>
-            <div class="hero-title">
-                <h1>Recruit</h1>
-            </div>
-        </div>
-    </section>
+@endsection
+
+@section('title', ' | Recruit')
+
+@section('hero_title', 'Recruit')
+    
+@section('content')
     <section class="main-contents w-container narrow">
         <div class="head-text-container">
             <h2>People</h2>
@@ -122,13 +98,4 @@
             <p>受付時間 10:00〜19:00（採用担当まで）</p>
         </div>
     </section>
-    <footer>
-        <ul class="footer-menu">
-            <li><a href="../contact/contact.html">CONTACT</a></li>
-            <li><a href="../recruit/recruit.html">RECRUIT</a></li>
-            <li><a href="../privacy_policy/privacy_policy.html">PRIVACY POLICY</a></li>
-        </ul>
-        <p><i class="far fa-copyright"></i>2021 Jun Takizawa</p>
-    </footer>
-</body>
-</html>
+@endsection

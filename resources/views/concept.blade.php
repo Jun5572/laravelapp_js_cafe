c525e366ca438f319b95f7fb3a6456264930f11c
= <takizawa.tab@gmail.com>
2021-12-02 23:15:49 +0900

    スタイル微調整

--- resources/views/concept.blade.php
+++ resources/views/concept.blade.php
@@ -18,7 +18,7 @@
         <p>テキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入ります</p>
     </div>
 </section>
-<section class="main-visual-img">
+<section class="main-visual-img w-container">
     <div class="text-container">
         <h3 class="title">ABOUT J's Cafe</h3>
         <p>テキストが入りますテキストが入りますテキストが入ります</p>
@@ -26,7 +26,7 @@
         <p>テキストが入りますテキストが入りますテキストが入ります</p>
     </div>
 </section>
-<section class="commitment-container">
+<section class="commitment-container w-container">
     <img class="commit-img" src="{{ asset('images/pablo-merchan-montes-SCbq6uKCyMY-unsplash.jpg') }}">
     <img class="commit-img" src="{{ asset('images/luke-porter-ud6XcK_MUGI-unsplash.jpg') }}">
     <div class="text-container">

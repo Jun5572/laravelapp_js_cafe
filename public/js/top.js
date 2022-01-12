1df7f8c3d959c50f9def15a13e7d87da9cdb8db6
= <takizawa.tab@gmail.com>
2022-01-12 14:19:28 +0900

    ローディング待機時間設定

--- public/js/top.js
+++ public/js/top.js
@@ -8,17 +8,21 @@ window.addEventListener("load", function () {
   var outer_wrapper = document.querySelector('.outer-wrapper');
   var sns_wrapper = document.querySelector('.sns-wrapper');
   new Promise(function (resolve) {
-    loading_wrapper.classList.add('done'); // resolveで処理が終わったことを伝える
+    setTimeout(function () {
+      loading_wrapper.classList.add('done');
+    }, 3000); // resolveで処理が終わったことを伝える
 
     resolve();
   }).then(function () {
     // ページ読み込み完了後にフェードイン
-    outer_wrapper.classList.add('loaded');
+    setTimeout(function () {
+      outer_wrapper.classList.add('loaded');
+    }, 3000);
   }).then(function () {
     setTimeout(function () {
       // top画面　snsのエリアをページ読み込み後に表示 
       sns_wrapper.classList.add('show');
-    }, 1000);
+    }, 4000);
   });
 }, false);
 /******/ })()

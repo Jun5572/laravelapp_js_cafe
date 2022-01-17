54073f51c8ff98715056c387cc53188da18ad9b5
= <takizawa.tab@gmail.com>
2022-01-18 00:15:01 +0900

    ロゴのスタイル編集中

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,4 +1,6 @@
-<a href="{{ url('/') }}" class="logo">logo</a>
+<a href="{{ url('/') }}" class="logo">
+    {{-- <img src="{{ asset('images/js_cafe_logo.png') }}" alt="siteLogo"> --}}
+</a>
 <nav>
     <ul>
         <li><a href="{{ url('/news') }}">NEWS</a></li>
